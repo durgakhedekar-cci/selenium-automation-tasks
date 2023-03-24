290118673140e8498192d13a144c501bb990116c
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-24 10:34:12 +0530

    Created conftest.py file

--- SeleniumQAApp/test_invalid_login.py
+++ SeleniumQAApp/test_invalid_login.py
@@ -1,19 +1,7 @@
 import time
+
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-@pytest.fixture
-def driver():
-    # open the browser
-    print("Creating driver for chrome browser")
-    browser_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    yield browser_driver
-    print("Closing driver for chrome browser")
-    browser_driver.quit()
 
 
 @pytest.mark.invalid
