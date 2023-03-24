842375459a837051eac4dcf89646baa6dd60da38
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-24 12:10:39 +0530

    Removed the actual username and password from the TC

--- Greythr/greythr.py
+++ Greythr/greythr.py
@@ -10,10 +10,10 @@ driver.get("https://creativecapsule.greythr.com/")
 time.sleep(2)
 
 login_name = driver.find_element(By.NAME, "username")
-login_name.send_keys("CCI00224")
+login_name.send_keys("username")
 
 password = driver.find_element(By.ID, "password")
-password.send_keys("Alps123456!!***")
+password.send_keys("password")
 time.sleep(2)
 login_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Log in')]")
 login_btn.click()
