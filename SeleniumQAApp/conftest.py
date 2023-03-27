945617f5b7463849857a4d1bd544bd3fd4a61960
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-27 11:07:20 +0530

    Tried mozilla and edge browser and ran the tests

--- SeleniumQAApp/conftest.py
+++ SeleniumQAApp/conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -12,17 +14,23 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 # this file will have code for arrange only
 @pytest.fixture
-def driver():
+def driver(request):
+    browser = request.config.getoption("--browser")
     # open the browser
-    print("Creating driver for chrome browser")
-    # browser_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    # browser_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    browser_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    print(f"Creating driver for {browser} browser")
+    if browser == "chrome":
+        browser_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser == "firefox":
+        browser_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif browser == "edge":
+        browser_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    else:
+        raise TypeError(f"Expected browser to be chrome, firefox, edge got {browser}")
+
     yield browser_driver
-    print("Closing driver for chrome browser")
+    print(f"Closing driver for {browser} browser")
     browser_driver.quit()
 
 
-
-
-
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)")
