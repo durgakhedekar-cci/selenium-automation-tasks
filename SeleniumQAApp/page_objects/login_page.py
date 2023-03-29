2ccdaefe557332eef1317e3bb44f670543c9a40d
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-29 10:11:11 +0530

    Made changes as per the session for the page object

--- SeleniumQAApp/page_objects/login_page.py
+++ SeleniumQAApp/page_objects/login_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
@@ -7,8 +9,9 @@ class LoginPage:
     __username_field = (By.ID, "username_textbox")
     __password_field = (By.ID, "password_textbox")
     __submit_button = (By.XPATH, "/html//div[@id='root']/div[@class='App']/section/div//form//button[@type='submit']")
+    __error_label_field = (By.CSS_SELECTOR, ".mb-2.text-center.text-danger")
 
-    def __init__(self, driver:WebDriver):
+    def __init__(self, driver: WebDriver):
         self._driver = driver
 
     def open(self):
@@ -18,3 +21,22 @@ class LoginPage:
     def current_url(self):
         return self._driver.current_url
 
+    def perform_login(self, username, password):
+        username_data = self._driver.find_element(*self.__username_field)
+        password_data = self._driver.find_element(*self.__password_field)
+        submit_button_field = self._driver.find_element(*self.__submit_button)
+
+        username_data.send_keys(username)
+        password_data.send_keys(password)
+        time.sleep(2)
+        submit_button_field.click()
+
+    @property
+    def get_error_label_text(self):
+        error_label = self._driver.find_element(self.__error_label_field)
+        actual_text = error_label.text
+        return actual_text
+
+    def is_error_label_displayed(self) -> bool:
+        error_label = self._driver.find_element(self.__error_label_field)
+        return error_label.is_displayed()
