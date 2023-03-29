c2f4b33e17c5045775805f18e34cafd08fb22874
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-29 12:56:35 +0530

    The test invalid TC has been changed. Made all the changes shown in page object session 1

--- SeleniumQAApp/test_invalid_login.py
+++ SeleniumQAApp/test_invalid_login.py
@@ -3,35 +3,20 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
+from page_objects.login_page import LoginPage
 
-@pytest.mark.login
+
+
 @pytest.mark.negative
 @pytest.mark.parametrize("username,password,expected_error_message",
                          [("adminxyz", "admin123", "Invalid Credentials"),
                           ("admin", "admin12345", "Invalid Credentials"),
                           ("adminsss", "admin12345s", "Invalid Credentials")])
-def test_invalid_username_login(driver, username, password, expected_error_message):
-    driver.get("https://login-app-iota.vercel.app/login")
-    time.sleep(2)
-    current_url = driver.current_url
-    assert current_url == "https://login-app-iota.vercel.app/login", "Default URL should be login"
+def test_invalid_username_login_p(driver, username, password, expected_error_message):
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.perform_login(username, password)
 
-    user_name = driver.find_element(By.ID, "username_textbox")
-
-    passwords = driver.find_element(By.ID, "password_textbox")
-
-    login_btn = driver.find_element(By.XPATH,
-                                    "/html//div[@id='root']/div[@class='App']/section/div//form//button[@type='submit']")
-
-    user_name.send_keys(username)
-    passwords.send_keys(password)
-    login_btn.click()
-    # # Validate logged in URL
-    logging_url = driver.current_url
-    assert logging_url == "https://login-app-iota.vercel.app/login", "URL should not change"
-    # Validate login message
-    error_text = driver.find_element(By.CSS_SELECTOR, ".mb-2.text-center.text-danger")
-    error_txt = error_text.text
-    assert error_text.is_displayed(), "Error message is not displayed"
-    assert error_txt == expected_error_message, "Error message does not match"
-
+    assert login_page.current_url == "https://login-app-iota.vercel.app/login", "URL should not change"
+    #assert login_page.is_error_label_displayed(), "Error message is not displayed"
+    #assert login_page.error_label_text == expected_error_message, "Error message not displayed"
