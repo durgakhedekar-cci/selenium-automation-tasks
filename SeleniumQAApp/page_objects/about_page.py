5ecfaca62bffd924334795e17218c8120b887109
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-31 15:42:55 +0530

    Created the page object for about_page

---
+++ SeleniumQAApp/page_objects/about_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+
+
+class AboutPage:
+    __contact_pg_url = (By.XPATH, "//div[@id='navbarSupportedContent']//a[@href='/about']")
+    __header_txt = (By.TAG_NAME, "h1")
+
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+
+    @property
+    def current_url(self):
+        return self._driver.current_url
+
+    def navigate_to_about(self):
+        self._driver.find_element(*self.__contact_pg_url).click()
+
+    @property
+    def header_text_data(self) -> str:
+        head_text = self._driver.find_element(self.__header_txt)
+        actual_txt = head_text.text
+        return actual_txt
+
+    def is_header_text_displayed(self) -> bool:
+        head_txt = self._driver.find_element(self.__header_txt)
+        return head_txt.is_displayed()
