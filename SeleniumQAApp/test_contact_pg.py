9359b3e1a598c72cb58d080203c9c443a36939ff
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-03-31 15:44:24 +0530

    Created a separate TC for contact page

---
+++ SeleniumQAApp/test_contact_pg.py
@@ -0,0 +1,19 @@
+import pytest
+from page_objects.about_page import AboutPage
+from page_objects.login_page import LoginPage
+
+@pytest.mark.contact
+def test_contact_pg(driver):
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.perform_login('admin', 'admin123')
+
+    contact_page = AboutPage(driver)
+    contact_page.navigate_to_about()
+    assert contact_page.current_url == "https://login-app-iota.vercel.app/about", "URL is not correct"
+
+    contact_page.header_text_data() == "Welcome to Selenium Learning Group", "title does not match"
+    #contact_page.is_header_text_displayed()
+
+
+
