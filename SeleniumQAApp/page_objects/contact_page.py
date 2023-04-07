bd0dbf59e2f1160b3994e7d57f610cf5a8038ebb
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-04-07 16:20:26 +0530

    Started automating the Tcs

--- SeleniumQAApp/page_objects/contact_page.py
+++ SeleniumQAApp/page_objects/contact_page.py
@@ -2,9 +2,10 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
 
-class ContactPage():
+class ContactPage:
     __contact_url = (By.XPATH, "//div[@id='navbarSupportedContent']//a[@href='/contact']")
-
+    __add_contacts_text = (By.XPATH, "/html//div[@id='root']//div[@role='button']/div[@class='text-uppercase']")
+    __plus_btn = (By.CSS_SELECTOR, ".fs-2.pointer-event > path:nth-of-type(1)")
 
     def __init__(self, driver: WebDriver):
         self._driver = driver
@@ -16,6 +17,16 @@ class ContactPage():
     def current_url(self):
         return self._driver.current_url
 
+
     def go_to_contact_pg(self):
         self._driver.find_element(*self.__contact_url).click()
+
+    @property
+    def get_the_heading(self) -> str:
+        header_text = self._driver.find_element(*self.__add_contacts_text)
+        actual_txt = header_text.text
+        return actual_txt
+
+    def click_plus_button(self):
+        self._driver.find_element(*self.__plus_btn).click()
 
--- SeleniumQAApp/test_contact.py
+++ SeleniumQAApp/test_contact.py
@@ -11,5 +11,9 @@ def test_contact(driver):
 
     contact_page = ContactPage(driver)
     contact_page.go_to_contact_pg()
-    time.sleep(2)
+
+    assert contact_page.current_url == "https://login-app-iota.vercel.app/contact", "URL is not correct"
+    assert contact_page.get_the_heading == "ADD CONTACTS", " The heading is incorrect"
+
+    contact_page.click_plus_button()
 
