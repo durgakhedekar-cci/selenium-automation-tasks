181781f0748ddbb986a8541d494df2a39db208fc
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-04-21 09:54:05 +0530

    Working on the TCs

--- SeleniumQAApp/test_testcases.py
+++ SeleniumQAApp/test_testcases.py
@@ -20,10 +20,31 @@ def test_testcases(driver):
     testcase_page.check_collapse_expand_tc4()
 
     assert testcase_page.get_text_tc1 == "Test 1 : A user with valid credentials should able to Login successfully\nURL : https://login-app-iota.vercel.app/login", "The text is incorrect"
-    assert testcase_page.get_inside_text_tc1 == "1. Open Browser", "The text is incorrect\n2. Navigate to Site URL"
+    assert testcase_page.get_inside_text_tc1 == ('1. Open Browser\n' '2. Navigate to Site URL\n' '3. Validate if default URL is pointing to login route\n'
+                                                '4. Locate username element\n'
+                                                '5. Locate password element\n'
+                                                '6. Locate Login button\n'
+                                                '7. Enter Valid username\n'
+                                                '8. Enter valid password\n'
+                                                '9. Click on login button\n'
+                                                '10. Validate logged in URL\n'
+                                                '11. Validate login message'), "The text is incorrect"
 
     assert testcase_page.get_text_tc2 == "Test 2 : A user with valid credentials should able to Login successfully and logout successfully\nURL : https://login-app-iota.vercel.app/login", "The text is incorrect"
+    assert testcase_page.get_inside_text_tc2 == ('1. Open Browser\n' '2. Navigate to Site URL\n' '3. Validate if default URL is pointing to login route\n'
+                                                '4. Locate username element\n'
+                                                '5. Locate password element\n'
+                                                '6. Locate Login button\n'
+                                                '7. Enter Valid username\n'
+                                                '8. Enter valid password\n'
+                                                '9. Click on login button\n'
+                                                '10. Validate logged in URL\n'
+                                                '11. Validate login message\n'
+                                                '12. Locate logout menu/button\n'
+                                                '13. Click on logout button\n'
+                                                '14. Validate that login page URL is displayed'), "The text is incorrect"
 
     assert testcase_page.get_text_tc3 == "Test 3 : A user with invalid username should not be able to Login\nURL : https://login-app-iota.vercel.app/login" , "The text is incorrect"
+
 
     assert testcase_page.get_text_tc4 == "Test 4 : A user with invalid password should not be able to Login\nURL : https://login-app-iota.vercel.app/login", "The text is incorrect"
