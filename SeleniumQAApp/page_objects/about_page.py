db8a15a72a4c5cef60785f564b7a5d69445a9a2f
durgakhedekar <durga.khedekar@creativecapsule.com>
2023-05-31 09:32:49 +0530

    Revert "Made changes as one of the TC was failing"
    This reverts commit c7621e2a710c80d8396981c67bb653bd12fb9266.

--- SeleniumQAApp/page_objects/about_page.py
+++ SeleniumQAApp/page_objects/about_page.py
@@ -1,7 +1,6 @@
-import requests
-from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class AboutPage:
